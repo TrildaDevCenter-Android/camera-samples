a8aacfb0e06a183956468e1c971ddfbb9099212c
Paul Plaquette <paul.plaquette@gmail.com>
2024-01-23 16:25:39 +0100

    add more code to identify

--- CameraX-MLKit/app/src/main/java/com/example/camerax_mlkit/MainActivity.kt
+++ CameraX-MLKit/app/src/main/java/com/example/camerax_mlkit/MainActivity.kt
@@ -63,8 +63,24 @@ class MainActivity : AppCompatActivity() {
         val previewView: PreviewView = viewBinding.viewFinder
 
         val options = BarcodeScannerOptions.Builder()
-            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
-            .build()
+            .setBarcodeFormats(
+                Barcode.FORMAT_QR_CODE,
+                Barcode.FORMAT_AZTEC,
+                Barcode.FORMAT_CODE_128,
+                Barcode.FORMAT_CODE_39,
+                Barcode.FORMAT_CODE_93,
+                Barcode.FORMAT_EAN_8,
+                Barcode.FORMAT_EAN_13,
+                Barcode.FORMAT_QR_CODE,
+                Barcode.FORMAT_UPC_A,
+                Barcode.FORMAT_UPC_E,
+                Barcode.FORMAT_PDF417,
+                Barcode.FORMAT_CODABAR,
+                Barcode.FORMAT_ITF,
+                Barcode.FORMAT_UPC_A,
+                Barcode.FORMAT_UPC_E,
+            ).build()
+
         barcodeScanner = BarcodeScanning.getClient(options)
 
         cameraController.setImageAnalysisAnalyzer(
